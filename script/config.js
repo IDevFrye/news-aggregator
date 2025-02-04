35f31048c5aa2f204fe0355bd390ade5e163855c
Andrey Kostin <andrey.kostin.111@mail.ru>
2025-02-04 12:51:22 +0300

    [M3][TASK-8.2] Add config

---
+++ script/config.js
@@ -0,0 +1,3 @@
+export const API_KEY = "cece87a383334007a246c2ffa82ffa26";
+export const API_URL_HEADLINES = "https://newsapi.org/v2/top-headlines?country=";
+export const API_URL_SEARCH = "https://newsapi.org/v2/everything?q=";
